c2d7480bb1bd82508afe0a2c2d75a638340c83c7
Simone Gramsch <simone.gramsch@itwm.fraunhofer.de>
2026-06-15 11:03:02 +0200

    Update documentation

--- build/_shared/j-XIQ75IJR.js
+++ build/_shared/j-XIQ75IJR.js
@@ -1,1 +1,1 @@
-import{a}from"/build/_shared/chunk-AMSYD6KQ.js";import"/build/_shared/chunk-OZE3FFNP.js";export default a();
+import{a}from"/thma_anm_de/build/_shared/chunk-AMSYD6KQ.js";import"/thma_anm_de/build/_shared/chunk-OZE3FFNP.js";export default a();
--- build/_shared/wren-FGHU2HOT.js
+++ build/_shared/wren-FGHU2HOT.js
@@ -1,1 +1,1 @@
-import{a}from"/build/_shared/chunk-MOOSZ6D3.js";import"/build/_shared/chunk-OZE3FFNP.js";export default a();
+import{a}from"/thma_anm_de/build/_shared/chunk-MOOSZ6D3.js";import"/thma_anm_de/build/_shared/chunk-OZE3FFNP.js";export default a();
